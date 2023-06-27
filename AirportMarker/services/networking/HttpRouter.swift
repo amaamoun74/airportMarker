b385557f661d7a40a81fc666703e212db00f16fe
ahmed maamoun <amaamoun74@gmail.com>
2023-06-27 19:37:11 +0300

    network Layer

---
+++ AirportMarker/services/networking/HttpRouter.swift
@@ -0,0 +1,13 @@
+//
+//  HttpRouter.swift
+//  AirportMarker
+//
+//  Created by ahmed on 27/06/2023.
+//
+
+import Foundation
+protocol HttpRouter {
+    var baseUrlString : String {get}
+    var path: String {get}
+    
+}
