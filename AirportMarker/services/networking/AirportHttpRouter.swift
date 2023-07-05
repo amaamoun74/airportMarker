0e22f9696ec151b607d0793af4e3b4d6b19966b3
ahmed maamoun <amaamoun74@gmail.com>
2023-07-05 18:19:43 +0300

    network Layer

--- AirportMarker/services/networking/AirportHttpRouter.swift
+++ AirportMarker/services/networking/AirportHttpRouter.swift
@@ -5,12 +5,47 @@
 //  Created by ahmed on 04/07/2023.
 //
 
-import Foundation
+import Alamofire
 
 enum AirportHttpRouter {
     case getAirPorts
 }
 
 extension AirportHttpRouter: HttpRouter {
-    var 
+    var baseUrlString: String {
+        return ""
+    }
+    
+    var path: String {
+        switch self {
+        case .getAirPorts:
+            return "airport.json"
+        }
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self{
+        case .getAirPorts:
+            return .get
+        }
+    }
+    
+    var headers: Alamofire.HTTPHeaders? {
+        return nil
+    }
+    
+    var parameters: Alamofire.Parameters? {
+        return nil
+    }
+    
+    func body() throws -> Data? {
+        return nil
+    }
+    
+    func request(usingHttpService Service: HttpService) {
+        
+    }
+    
+    //var
+    
 }
